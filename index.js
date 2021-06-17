0bf6326f8caa23a2ab050fcf0e69daf8a2a3c7b7
Brandonv93 <bvicc93@gmail.com>
2021-06-17 11:21:18 -0500

    file changes
    added index.html to a views
    
    adjusted express server to look in views folder
    
    root path shows html

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require('express')
 const app = express();
 
 
-
+app.set('views', __dirname+'/views')
 app.use(express.json())
 app.use(express.urlencoded({useNewUrlParser:true}))
 
 //routing
 
 app.get('/', (req,res)=>{
-    res.send("i get bitches");
+    res.render('index')
 })
 
 app.listen(process.env.PORT, function(){
