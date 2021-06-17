f550d7b7f81675be9be1d486e873e275ec2c2a2a
Brandonv93 <bvicc93@gmail.com>
2021-06-17 11:25:56 -0500

    added ejs
    added ejs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 
-
+app.set('view engine', 'ejs') // allows us to render information to the front end
 app.set('views', __dirname+'/views')
 app.use(express.json())
 app.use(express.urlencoded({useNewUrlParser:true}))
